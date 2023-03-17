44a67a23c60ac2b2c4ed9aebc42bfca8584e21fa
rediska235 <vanya.bobr.12@gmail.com>
2023-03-17 20:52:51 +0300

    Add ExceptionsInitializer

---
+++ Fridges.Domain/Exceptions/ExceptionsInitializer.cs
@@ -0,0 +1,10 @@
+﻿namespace Fridges.Domain.Exceptions;
+
+public class ExceptionsInitializer
+{
+    public NotFoundException productNotFound = new("Product not found.");
+    public NotFoundException fridgeNotFound = new("Fridge not found.");
+
+    public AlreadyExistsException productAlreadyExists = new("Product with this name already exists.");
+    public AlreadyExistsException fridgeAlreadyExists = new("Fridge with this name already exists.");
+}
